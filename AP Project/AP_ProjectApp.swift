3232acdbd49aa905c286b607174966b89f2fee7a
gcbarnes23e <graceripleyred@gmail.com>
2023-03-10 09:09:35 -0600

    Initial Commit

---
+++ AP Project/AP_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AP_ProjectApp.swift
+//  AP Project
+//
+//  Created by Grace Barnes23 on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AP_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
